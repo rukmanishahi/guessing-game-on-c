e9918be6dba62206af781f313536730e9bfcd054
Rukmani Shahi <155186677+rukmanishahi@users.noreply.github.com>
2026-01-28 13:19:54 +0000

    nothing

--- guess.c
+++ guess.c
@@ -4,12 +4,10 @@
     void guess(){
         int n,random_number;
         random_number = (rand() % 10)+1;
-       
         while(1){
              printf("Enter the number:: ");
         scanf("%d",&n);
-            if(n>random_number){
-                printf("wrong! the correct number is smaller than the input\n");}
+            if(n>random_number){printf("wrong! the correct number is smaller than the input\n");}
             else if (n<random_number){printf("wrong! the correct number is greater than the input\n");}
             else {printf("that's right! the correct number was %d",random_number);
             break;}}}
